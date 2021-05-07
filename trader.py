66db58a988e0aeae64fd2351a2e6489131dff23e
ybkong98 <75230763+ybkong98@users.noreply.github.com>
2021-05-07 10:16:20 +0900

    Update trader.py

--- trader.py
+++ trader.py
@@ -2,54 +2,70 @@ import time
 import pyupbit
 import datetime
 import numpy as np
+import pandas
 
 access = "key"
 secret = "key"
 
-def get_ror(k=0.5):
-    df = pyupbit.get_ohlcv("KRW-BTT",count=24,interval="minute60")
+tickers=pyupbit.get_tickers(fiat="KRW")
+
+def get_ror(ticker="KRW-BTC",k=0.5):
+    #백테스팅으로 k값에 따른 수익률 산출
+    df = pyupbit.get_ohlcv(ticker, count=7,interval="minute60")
     df['range'] = (df['high'] - df['low']) * k
     df['target'] = df['open'] + df['range'].shift(1)
 
-    fee = 0.0005
     df['ror'] = np.where(df['high'] > df['target'],
-                         df['close'] / df['target'] - fee,
+                         df['close'] / df['target'],
                          1)
 
     ror = df['ror'].cumprod()[-2]
     return ror
 
-
-def get_k():
-    result=0.5
-    M=0
+def get_k(ticker="KRW-BTC"):
+    #백테스팅 기반, 최선의 변동성 계수 산출
+    finalK,maxRor=0,0
     for k in np.arange(0.1, 1.0, 0.1):
-     ror = get_ror(k)
-     if ror>M:
-         M=ror
-         result=k
-    return result
+        ror = get_ror(ticker,k)
+        if maxRor<ror:
+            finalK,maxRor=k,ror
+    return finalK,maxRor
+    
+def get_best_coin(coins=tickers):
+    resultCoin,resultK=None,0
+    maxror=0
+    for coin in coins:
+        k,ror=get_k(ticker=coin)
+        target_price=get_target_price(ticker=coin, k=k)
+        current_price=get_current_price(ticker=coin)
+        ma15=get_ma15(ticker=coin)
+        if target_price<current_price or ma15<=current_price:
+            continue
+        if target_price/current_price>maxror:
+            maxror,resultCoin,resultK=target_price/current_price,coin,k
+        time.sleep(0.4)
+    return resultCoin,resultK
 
 def get_target_price(ticker, k):
-    """get target price"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=72)
+    #변동성 돌파 전략으로 매수 목표가 조회
+    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
-    """시작 시간 조회"""
+    #시작 시간 조회
     df = pyupbit.get_ohlcv(ticker, interval="minute60", count=1)
     start_time = df.index[0]
     return start_time
 
 def get_ma15(ticker):
-    """15시간 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute60", count=15)
+    #15시간 평균
+    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=15)
     ma15 = df['close'].rolling(15).mean().iloc[-1]
     return ma15
 
 def get_balance(ticker):
-    """get account info"""
+    #잔고 조회
     balances = upbit.get_balances()
     for b in balances:
         if b['currency'] == ticker:
@@ -59,32 +75,54 @@ def get_balance(ticker):
                 return 0
 
 def get_current_price(ticker):
-    """get current price of ticker"""
+    """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
-# login
+# 로그인
 upbit = pyupbit.Upbit(access, secret)
-print("autotrade start")
+print("Login done")
 
-# start trading
+
+ticker=None
+
+# 자동매매 시작
 while True:
     try:
+        tickers=pyupbit.get_tickers(fiat="KRW")
+        #코인, 변동성 상수 탐색
+        if ticker is None:
+            ticker,k=get_best_coin(coins=tickers)
+        
+        #거래시간설정
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTT")
+        start_time = get_start_time(ticker)
         end_time = start_time + datetime.timedelta(hours=1)
+        
+        print("coin=",ticker,"k=",k)
+        if start_time < now < end_time - datetime.timedelta(seconds=300):
+            target_price = get_target_price(ticker, k)
+            ma15 = get_ma15(ticker)
+            current_price = get_current_price(ticker)
+            print(target_price/current_price,ma15,current_price)
+            if target_price > 1.011*current_price and ma15 < current_price:
+                if start_time==get_start_time(ticker=ticker):
+                    krw = get_balance("KRW")
+                    if krw > 5000:
+                        print("Buy Order ",ticker)
+                        upbit.buy_market_order(ticker, krw*0.9995)
+                        buy=ticker
+                else:
+                    coin = get_balance(buy[4:])*get_current_price(buy)
+                    if coin > 5000:
+                        print("Sell Order ",ticker)
+                        upbit.sell_market_order(buy, coin*0.9995)
+            ticker=None
 
-        if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTT", get_k())
-            ma15 = get_ma15("KRW-BTT")
-            current_price = get_current_price("KRW-BTT")
-            if target_price < current_price and ma15 < current_price:
-                krw = get_balance("KRW")
-                if krw > 5000:
-                    upbit.buy_market_order("KRW-BTT", krw*0.9995)
         else:
-            BTT = get_current_price("KRW-BTT")*get_balance("KRW-BTT")
-            if BTT > 5000:
-                upbit.sell_market_order("KRW-BTT", BTT*0.9995)
+            coin = get_balance(buy[4:])*get_current_price(buy)
+            if coin > 5000:
+                print("Sell Order ",ticker)
+                upbit.sell_market_order(buy, coin*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
